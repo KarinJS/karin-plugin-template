f3b83dffc460d4f8fc32e00a855ac167c4ee3b16
Lain. <1072411694@qq.com>
2024-04-14 13:50:11 +0800

    fix：中文路径...

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ import { logger, common } from '#Karin'
 const filePath = common.absPath(import.meta.url.replace(/^file:(\/\/\/|\/\/)/, ''))
 /** 插件包的目录路径 */
 const dirname = path.dirname(filePath)
+/** 插件包的名称 */
+const basename = path.basename(dirname)
+/** 插件包相对路径 */
+const dirPath = './plugins/' + basename
 
-export { dirname }
+export { dirPath }
 
 logger.info('karin-plugin-template 插件 0.0.1初始化~')
